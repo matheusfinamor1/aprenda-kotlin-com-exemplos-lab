32154053581d1fb7573a88b91c0893e97a30d9c2
Matheus Finamor Correa <130673683+matheusfinamor1@users.noreply.github.com>
2023-07-06 00:03:33 -0300

    Ajustes na implementacao fornecida

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,63 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class NivelDaFormacao { BRONZE, PRATA, OURO }
 
-class Usuario
+enum class NivelDoAluno { BASICO, INTERMEDIARIO, AVANÇADO }
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class Usuario(val nome: String, val idade: Int, var nivel: NivelDoAluno)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class ConteudoEducacional(val nome: String, var duracao: Int)
+
+data class Formacao(
+    val nome: String,
+    val conteudos: List<ConteudoEducacional>,
+    val usuarios: List<Usuario>,
+    var nivelDaFormacao: NivelDaFormacao) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
+    
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
+    val usuarios: MutableList<Usuario> =
+    	mutableListOf(
+            Usuario(
+                nome = "Matheus",
+                idade = 20,
+                NivelDoAluno.INTERMEDIARIO
+            ),
+            Usuario(
+                nome = "Ana",
+                idade = 29,
+                NivelDoAluno.BASICO
+            )
+        )
+    
+    val conteudosEducacionais: MutableList<ConteudoEducacional> =
+    mutableListOf(
+        ConteudoEducacional(
+            nome = "LP",
+            duracao = 35
+        ),
+    	ConteudoEducacional(
+            nome = "IA",
+         	duracao = 60
+        )
+    )
+    
+    
+    val formacao = Formacao(
+        "Ciência da Computacao",
+        conteudosEducacionais,
+        usuarios,
+        NivelDaFormacao.PRATA
+    )
+    println("$formacao")
+    
+    
 }
